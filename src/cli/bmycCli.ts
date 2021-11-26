623ed5e4651b2cbf0825e5355eb7edf045e5079f
Jordan GAZEAU <jordan.gazeau@gmail.com>
2021-11-27 00:43:27 +0100

    fix: wrap fixed size and usage of unicode in usage string

--- src/cli/bmycCli.ts
+++ src/cli/bmycCli.ts
@@ -1,5 +1,6 @@
 import kleur = require('kleur');
 import {hideBin} from 'yargs/helpers';
+import {getOutputWidth} from '../utils/helpers';
 import {IArgumentsParser, ICliArguments} from './iArgumentsParser';
 import {CLI_USAGE, DEFAULT_CONFIGURATION_FILE_NAME} from '../utils/const';
 const yargs = require('yargs');
@@ -67,7 +68,7 @@ export class BmycCli {
           group: this.GROUPS.COMMONS,
         },
       })
-      .wrap(null)
+      .wrap(getOutputWidth())
       .epilog(
         `Additional information:
   GitHub: ${kleur.green('https://github.com/jgazeau/bmyc.git')}
--- src/utils/const.ts
+++ src/utils/const.ts
@@ -5,13 +5,13 @@ export const MAX_TTY_LENGTH = 100;
 export const NOT_AVAILABLE = 'N/A';
 
 export const BMYC_HEADER = `
- _____                      
-| ___ \\                     
-| |_/ /_ __ ___  _   _  ___ 
-| ___ \\ '_ \` _ \\| | | |/ __|
-| |_/ / | | | | | |_| | (__ 
-|____/|_| |_| |_|\\__, |\\___|
-                 |___/      
+\u25CF       _____                      
+\u25CF      | ___ \\                     
+\u25CF      | |_/ /_ __ ___  _   _  ___ 
+\u25CF      | ___ \\ '_ \` _ \\| | | |/ __|
+\u25CF      | |_/ / | | | | | |_| | (__ 
+\u25CF      |____/|_| |_| |_|\\__, |\\___|
+\u25CF                       |___/      
 `;
 
 export const CLI_USAGE = `${BMYC_HEADER}
